f32a05faf223d1b00c778f5f035ce86333f8a0ca
Lc163 <m13661203984@163.com>
2016-10-31 16:21:59 +0800

    first commit

--- New-daishu/js/js.js
+++
@@ -1,5 +0,0 @@
-$(".m1 dl").on("tap",function(){
-  	alert(1)
-    $(this).addClass("xzbk").siblings().removeClass("xzbk");
-    $(this).find('span img').attr("src","../images/qrchanpinxz.png").parents("dl").siblings().find("span img").attr("src","../images/qrchanping_03.png");
-})
